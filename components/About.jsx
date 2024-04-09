6eae41bf05217bc1a05de44288e73b07b5c61677
Keshav <keshav2021csds116@abesit.edu.in>
2024-04-10 03:07:49 +0530

    feat:About Page

---
+++ components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import localFont from "next/font/local";
+import Image from "next/image";
+import AboutImg from "@/assets/images/aboutSide.png";
+
+const pricedown = localFont({
+    src: './../assets/fonts/pricedown.ttf',
+})
+
+const About = () => {
+  return (
+    <div
+      className={`text-6xl bg-[url(/about.png)] bg-cover md:bg-cover bg-no-repeat h-screen flex justify-center items-center w-full`}
+    >
+      <div className="md:w-1/3">
+        <div className="hidden md:block">
+          <Image src={AboutImg} alt="err" width={600} height={600} />
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full md:w-2/3 px-5 text-left">
+        <div className={`text-5xl font-bold ${pricedown.className} `}>
+          What is Hacknovate 5.0 ?
+        </div>
+
+        <div className="text-base flex flex-col gap-5 py-4">
+          <span>
+            After the triumphant success of four consecutive years of
+            Hacknovate, ABES Institute of Technology proudly presents the
+            much-anticipated HACKNOVATE 5.0!
+          </span>
+
+          <span>
+            Hacknovate 5.0 aims to bring together the most brilliant
+            programmers, engineers, architects, designers, and business
+            visionaries for another extraordinary event. Our primary objective
+            is to foster an environment where the sharpest minds collaborate to
+            conceive the most innovative solutions.
+          </span>
+
+          <span>
+            Building upon the achievements of its predecessors, Hacknovate 5.0
+            will continue to be a nexus of creativity, pushing the boundaries of
+            what is possible in the realms of technology and innovation. In
+            recognition of the changing times, the Hybrid Mode of Hacknovate 5.0
+            will be implemented, opening doors for passionate young minds who
+            are unable to join us in person
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
