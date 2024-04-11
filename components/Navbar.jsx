df011966d3736c9cc4b3693e1f16577195db87e1
Keshav <keshav2021csds116@abesit.edu.in>
2024-04-11 12:10:05 +0530

    fix:About Page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -134,12 +134,12 @@ const Navbar = () => {
                     ))}
                 </div>
 
-                {/* Pre-Register Button */}
-                <Link href="/register" className='text-center mx-auto mt-2 md:mx-0'>
+                {/* Sponsor Button */}
+                <Link href="mailto:hacknovate@abesit.edu.in" className='text-center mx-auto mt-2 md:mx-0'>
                     <button className="relative inline-flex h-8 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                            Pre-Register
+                            Sponsor Us
                         </span>
                     </button>
                 </Link>
