930f516c63670700e9625df7f23a116378dcaa03
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-11 11:03:18 +0530

    Refactor Prize component and update styling in Prize.jsx

--- components/Prize.jsx
+++ components/Prize.jsx
@@ -1,5 +1,11 @@
 import Heading from './Heading'
 import { motion } from 'framer-motion'
+import { Anton } from 'next/font/google'
+
+const anton = Anton({
+  subsets: ["latin-ext"],
+  weight: ["400"],
+});
 
 const Prizes = () => {
   return (
@@ -13,28 +19,7 @@ const Prizes = () => {
 
       <Heading>Prizes</Heading>
 
-      <div className='my-4 flex flex-col sm:flex-row justify-evenly overflow-y-auto scroll-snap-y-mandatory'>
-
-        <div className="bg-[url(/hero.jpg)] bg-cover bg-center h-screen flex items-center justify-center w-full sm:w-2/5 rounded-md border flex-col scroll-snap-start" >
-
-
-
-          <img src="./Frame 2.jpg" alt="reload" className='flex justify-center align-middle items-center border border-gray-700 rounded-md w-full md:w-3/4 lg:w-1/2' />
-          <div>
-            <Heading>Hacknovate Online</Heading>
-            <ul className='my-3 flex flex-col'>
-              <li className='font-semibold text-2xl'>Winner: 200$ </li>
-              <li className='font-semibold text-2xl'>Runner Up: 100$ </li>
-              <li className='font-semibold text-2xl'>Second Runner Up: 50$ </li>
-            </ul>
-          </div>
-
-
-          <button className='px-6 py-2 text-sm sm:px-12 sm:py-3 sm:text-base md:px-24 md:py-4 md:text-xl hover:bg-white hover:text-black justify-center font-semibold border border-white m-5'>Register Now</button>
-
-
-
-        </div>
+      <div className='my-4 flex flex-col sm:flex-row gap-y-6 justify-evenly overflow-y-auto scroll-snap-y-mandatory'>
 
 
         <div className="bg-[url(/map2.jpeg)] bg-center h-screen flex items-center justify-center w-full sm:w-2/5 rounded-md border flex-col .bg-50 scroll-snap-start" >
@@ -43,24 +28,43 @@ const Prizes = () => {
           <img src="./Frame 2.jpg" alt="reload" className='flex justify-center align-middle items-center border border-gray-700 rounded-md w-full md:w-3/4 lg:w-1/2' />
           <div>
             <Heading>Hacknovate Offline</Heading>
-            <ul className='my-3 flex flex-col'>
-              <li className='font-semibold text-2xl'>Winner: 500$ </li>
-              <li className='font-semibold text-2xl'>Runner Up: 200$ </li>
-              <li className='font-semibold text-2xl'>Second Runner Up: 100$ </li>
+            <ul className={`my-3 flex flex-col ${anton.className} tracking-widest`}>
+              <li className='font-semibold text-2xl'>Winner: ₹25,000 </li>
+              <li className='font-semibold text-2xl'>Runner Up: ₹15,000 </li>
             </ul>
           </div>
 
-          <button className='px-6 py-2 text-sm sm:px-12 sm:py-3 sm:text-base md:px-24 md:py-4 md:text-xl hover:bg-white hover:text-black justify-center font-semibold border border-white m-5'>Register Now</button>
+          <button className={`px-6 py-2 text-sm sm:px-12 sm:py-3 sm:text-base md:px-24 md:py-4 md:text-xl hover:bg-white hover:text-black justify-center font-semibold border border-white m-5 backdrop-blur-sm rounded ${anton.className}`}>Register Now</button>
+
+
+
+        </div>
+        <div className="bg-[url(/hero.jpg)] bg-cover bg-center h-screen flex items-center justify-center w-full sm:w-2/5 rounded-md border flex-col scroll-snap-start" >
+
+
+
+          <img src="./Frame 2.jpg" alt="reload" className='flex justify-center align-middle items-center border border-gray-700 rounded-md w-full md:w-3/4 lg:w-1/2' />
+          <div>
+            <Heading>Hacknovate Online</Heading>
+            <ul className={`my-3 flex flex-col ${anton.className} tracking-widest`}>
+              <li className='font-semibold text-2xl'>Winner: ₹10,000 </li>
+              <li className='font-semibold text-2xl'>Runner Up: ₹5,000 </li>
+            </ul>
+          </div>
+
+
+          <button className={`px-6 py-2 text-sm sm:px-12 sm:py-3 sm:text-base md:px-24 md:py-4 md:text-xl hover:bg-white hover:text-black justify-center font-semibold border border-white m-5 backdrop-blur-sm rounded ${anton.className}`}>Register Now</button>
 
 
 
         </div>
 
 
+
       </div>
 
-      <div className='flex justify-center align-middle items-center border border-white w-full sm:w-3/4 md:w-2/5 mx-auto my-5 py-4'>
-        <h1 className='font-bold text-xl'>Winning Stakes: 500K and Beyond!</h1>
+      <div className='flex justify-center items-center text-center border border-white w-full sm:w-3/4 md:w-2/5 mx-auto my-5 py-4'>
+        <h2 className={`font-bold text-xl ${anton.className} tracking-wider`}>Winning Stakes: ₹500K and Beyond!</h2>
       </div>
 
 
