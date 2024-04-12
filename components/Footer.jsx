5a3a911b59eadc56b08218a4948d4c6570df390f
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-12 09:41:35 +0530

    fix: jsx related warnings

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -66,10 +66,6 @@ function Footer() {
             <iframe
               width="100%"
               height="300"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
               src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=ABESIT+(Hacknovate%205.0)&amp;t=p&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
             ></iframe>
           </div>
