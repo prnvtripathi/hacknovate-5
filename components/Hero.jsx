a191365352529a80d5bdd4c757688240cdf5c870
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-12 10:35:03 +0530

    Update button styling and add logo image in Hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -55,7 +55,8 @@ export default function Hero() {
                             transition={{ duration: 0.4, delay: 1.3 }}
                             className={`flex flex-col md:flex-row gap-2 md:gap-x-8 w-11/12 ${anton.className}`}>
                             <Link href='https://hacknovate5.devfolio.co/'>
-                                <button className='bg-transparent w-full outline outline-1 py-3 px-7 rounded hover:bg-white hover:text-black transition-colors'>
+                                <button className='bg-transparent w-full outline outline-1 py-3 px-7 rounded hover:bg-white hover:text-black transition-colors flex items-center justify-center gap-2'>
+                                    <span className='text-xl'><Image src={'/devfolio.png'}  width={24} height={24} /></span>
                                     REGISTER
                                 </button>
                             </Link>
