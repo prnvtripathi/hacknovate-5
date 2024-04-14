9e089cafad015d038a8e7e60a5dee9b34524b66a
Keshav <keshav2021csds116@abesit.edu.in>
2024-04-14 14:26:09 +0530

    Update:Community Partner Page

--- components/CommPartners.jsx
+++ components/CommPartners.jsx
@@ -1,45 +1,106 @@
-import Image from 'next/image';
-import Heading from './Heading';
-import { Anton } from 'next/font/google';
+import React, { useState } from "react";
+import Image from "next/image";
+import Heading from "./Heading";
+import { Anton } from "next/font/google";
+import { CommunityPartnerR1 } from "@/data/communityPartner";
+import styled, { keyframes, css } from "styled-components";
 
 const anton = Anton({
-    subsets: ['latin-ext'],
-    weight: ["400"],
-})
-
-const data = [
-    {
-        name: "GDSC ABESIT",
-        img: "/comm/GDSC.png",
-    },
-    {
-        name: "IT Technical Club",
-        img: "/comm/ITTC.jpg",
-    },
-    {
-        name:  "Kalrav Club",
-        img: "/comm/kalrav.png",
-    },
-    {
-        name: "Aayaam Club",
-        img: "/comm/aayaam.png",
-    }
-]
+  subsets: ["latin-ext"],
+  weight: ["400"],
+});
 
 export default function CommPartners() {
-    return (
-        <div className="w-11/12 mx-auto">
-            <Heading>
-                Community Partners
-            </Heading>
-            <div className='grid md:grid-cols-3 place-content-center items-center'>
-                {data.map((item, index) => (
-                    <div key={index} className="flex flex-col items-center gap-5">
-                        <Image src={item.img} width={200} height={200} />
-                        <p className={`text-2xl ${anton.className}`}>{item.name}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-11/12 mx-auto">
+      <Heading>Community Partners</Heading>
+      <Wrapper>
+        <Marquee>
+          <MarqueeGroup>
+            {CommunityPartnerR1.map((item, index) => (
+              <ImageGroup key={index}>
+                <ImageContainer>
+                  <img className="" src={item.img} alt="err" />
+                </ImageContainer>
+              </ImageGroup>
+            ))}
+          </MarqueeGroup>
+          <MarqueeGroup>
+            {CommunityPartnerR1.map((item, index) => (
+              <ImageGroup key={index}>
+                <ImageContainer>
+                  <img className="" src={item.img} alt="err" />
+                </ImageContainer>
+              </ImageGroup>
+            ))}
+          </MarqueeGroup>
+        </Marquee>
+      </Wrapper>
+    </div>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Marquee = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: hidden;
+  user-select: none;
+  mask-image: linear-gradient(
+    to right,
+    hsl(0 0% 0% / 0),
+    hsl(0 0% 0% / 1) 10%,
+    hsl(0 0% 0% / 1) 90%,
+    hsl(0 0% 0% / 0)
+  );
+`;
+
+const ImageGroup = styled.div`
+  display: grid;
+  place-items: center;
+  width: 300px;
+`;
+
+const scrollX = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`;
+const ImageContainer = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  height: 150px;
+  padding: 10px;
+  margin-right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    height: 60px; /* Change height to 50px on small screens */
+    border-radius: 2px;  }
+`;
+
+const common = css`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  white-space: nowrap;
+  width: 100%;
+  animation: ${scrollX} 30s linear infinite;
+`;
+
+const MarqueeGroup = styled.div`
+  ${common}
+`;
---
+++ data/communityPartner.js
@@ -0,0 +1,20 @@
+const CommunityPartnerR1 = [
+    {
+        name: "GDSC ABESIT",
+        img: "/comm/gdsc_abesit.png",
+    },
+    {
+        name: "IT Technical Club",
+        img: "/comm/ITTC.jpg",
+    },
+    // {
+    //     name:  "Kalrav Club",
+    //     img: "/comm/kalrav.png",
+    // },
+    {
+        name: "Aayaam Club",
+        img: "/comm/aayaam.png",
+    }
+]
+export {CommunityPartnerR1}
+
