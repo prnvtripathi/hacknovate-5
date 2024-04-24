b7a00435c6882c93abd4aad687ea0654e5bd102d
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-24 22:17:26 +0530

    Update Sponsors component with new sponsor images and headings

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,9 +1,43 @@
-import Image from 'next/image'
+import Heading from './Heading'
+import localFont from 'next/font/local'
+
+const title = localFont({
+    src: './../assets/fonts/ChaletComprime-CologneSixty.ttf',
+})
 
 const Sponsors = () => {
     return (
-        <div className="h-screen flex justify-center items-center" id='sponsors'>
-            <Image src="/sponsors.svg" alt="Sponsors" width={1000} height={1000} className="" />
+        <div className='w-11/12 mx-auto mt-12'>
+            <Heading>Our Sponsors</Heading>
+
+            <div className='py-12'>
+                <h2 className={`text-2xl md:text-5xl text-center md:text-left text-yellow-500 mb-4 ${title.className}`}>
+                    Gold Sponsors
+                </h2>
+                <div className='flex flex-col md:flex-row'>
+                    <img src='/sponsors/polygon.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                    <img src='/sponsors/ethindia.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto bg-white rounded-3xl p-2 object-contain' />
+                </div>
+            </div>
+            <div className='py-12'>
+                <h2 className={`text-2xl md:text-5xl text-center md:text-left mb-4 ${title.className}`}>
+                    General Sponsors
+                </h2>
+                <div className='flex flex-col md:flex-row'>
+                    <img src='/sponsors/interviewbuddy.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                    <img src='/sponsors/xyz.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto bg-white rounded-3xl p-2 object-contain' />
+                </div>
+            </div>
+            <div className='py-12'>
+                <h2 className={`text-2xl md:text-5xl text-center md:text-left mb-4 ${title.className}`}>
+                    Platform Sponsor
+                </h2>
+                <div className='flex flex-col md:flex-row'>
+                    <img src='/sponsors/devfolio.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                </div>
+            </div>
+
+
         </div>
     )
 }
