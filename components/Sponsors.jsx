90c46121dda09b1b2ba8effa625575f76dcd342c
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-26 10:02:07 +0530

    Update Sponsors component with new sponsor images and headings

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -20,12 +20,28 @@ const Sponsors = () => {
                 </div>
             </div>
             <div className='py-12'>
+                <h2 className={`text-2xl md:text-5xl text-center md:text-left text-gray-300 mb-4 ${title.className}`}>
+                    Silver Sponsors
+                </h2>
+                <div className='flex flex-col md:flex-row'>
+                    <img src='/sponsors/solcanvas.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                </div>
+            </div>
+            <div className='py-12'>
                 <h2 className={`text-2xl md:text-5xl text-center md:text-left mb-4 ${title.className}`}>
                     General Sponsors
                 </h2>
                 <div className='flex flex-col md:flex-row'>
                     <img src='/sponsors/interviewbuddy.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
                     <img src='/sponsors/xyz.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto bg-white rounded-3xl p-2 object-contain' />
+                </div>
+            </div>
+            <div className=''>
+                <h2 className={`text-2xl md:text-5xl text-center md:text-left mb-4 ${title.className}`}>
+                    Track Sponsors
+                </h2>
+                <div className='flex flex-col md:flex-row'>
+                    <img src='/sponsors/stindia.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
                 </div>
             </div>
             <div className='py-12'>
