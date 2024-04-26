2e3c5f9f786b232400ade04409fde32353bb63e4
Pranav Tripathi <tripathipranav14@gmail.com>
2024-04-26 17:30:45 +0530

    Update sponsor images and headings in Sponsors component

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -14,9 +14,9 @@ const Sponsors = () => {
                 <h2 className={`text-2xl md:text-5xl text-center md:text-left text-yellow-500 mb-4 ${title.className}`}>
                     Gold Sponsors
                 </h2>
-                <div className='flex flex-col md:flex-row'>
-                    <img src='/sponsors/polygon.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
-                    <img src='/sponsors/ethindia.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto bg-white rounded-3xl p-2 object-contain' />
+                <div className='flex flex-col md:flex-row gap-y-4'>
+                    <img src='/sponsors/polygon.png' alt='sponsor' className='w-60 mx-auto object-contain' />
+                    <img src='/sponsors/ethindia.png' alt='sponsor' className='w-60 mx-auto bg-white rounded-3xl p-2 object-contain' />
                 </div>
             </div>
             <div className='py-12'>
@@ -24,16 +24,16 @@ const Sponsors = () => {
                     Silver Sponsors
                 </h2>
                 <div className='flex flex-col md:flex-row'>
-                    <img src='/sponsors/solcanvas.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                    <img src='/sponsors/solcanvas.png' alt='sponsor' className='w-80 mx-auto object-contain' />
                 </div>
             </div>
             <div className='py-12'>
                 <h2 className={`text-2xl md:text-5xl text-center md:text-left mb-4 ${title.className}`}>
                     General Sponsors
                 </h2>
-                <div className='flex flex-col md:flex-row'>
-                    <img src='/sponsors/interviewbuddy.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
-                    <img src='/sponsors/xyz.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto bg-white rounded-3xl p-2 object-contain' />
+                <div className='flex flex-col md:flex-row gap-y-4'>
+                    <img src='/sponsors/interviewbuddy.png' alt='sponsor' className='w-60 mx-auto object-contain' />
+                    <img src='/sponsors/xyz.png' alt='sponsor' className='w-60 mx-auto bg-white rounded-3xl p-2 object-contain' />
                 </div>
             </div>
             <div className=''>
@@ -41,7 +41,7 @@ const Sponsors = () => {
                     Track Sponsors
                 </h2>
                 <div className='flex flex-col md:flex-row'>
-                    <img src='/sponsors/stindia.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                    <img src='/sponsors/stindia.png' alt='sponsor' className='w-60 mx-auto object-contain' />
                 </div>
             </div>
             <div className='py-12'>
@@ -49,7 +49,7 @@ const Sponsors = () => {
                     Platform Sponsor
                 </h2>
                 <div className='flex flex-col md:flex-row'>
-                    <img src='/sponsors/devfolio.png' alt='sponsor' className='w-1/2 md:w-1/4 mx-auto object-contain' />
+                    <img src='/sponsors/devfolio.png' alt='sponsor' className='w-60 mx-auto object-contain' />
                 </div>
             </div>
 
