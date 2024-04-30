2f298cb1ebfd9a74bfd263a0fd296b38e7693e5f
prateek tiwari <tiwariprateek1976@gmail.com>
2024-04-30 22:55:53 +0530

    added events page

---
+++ data/pastEvent.js
@@ -0,0 +1,19 @@
+export const PastEvents = [
+    {
+        id: 1,
+        title: "Tresure Hunt",
+        description: "A fun event to Kickstart the Hacknovate 5.0 !",
+        date: "27th Apr 2024",
+        img: 'https://res.cloudinary.com/dgozpfjis/image/upload/v1714495916/moydytr6uns0yuhhktlo.png'
+    },
+    {
+        id: 2,
+        title: "Meme Making Competition",
+        description: "Time to dominate the meme world with your creativity!",
+        date: "Ongoing",
+        link: 'https://discord.gg/rwMq2hBaXR',
+        img: "https://res.cloudinary.com/dgozpfjis/image/upload/v1714495835/zuhoiqre3hwqo5adzpwb.png"
+        
+    },
+    
+]
